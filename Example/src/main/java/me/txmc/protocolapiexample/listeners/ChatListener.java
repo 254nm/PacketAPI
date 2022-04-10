85bdc68013e2765b7002c5797ebd25542b9d2ae0
Nate Steven Legault <67813185+254nm@users.noreply.github.com>
2022-04-10 06:23:28 -0400

    Slightly more full example

--- Example/src/main/java/me/txmc/protocolapiexample/listeners/ChatListener.java
+++ Example/src/main/java/me/txmc/protocolapiexample/listeners/ChatListener.java
@@ -4,6 +4,7 @@ import me.txmc.protocolapi.PacketEvent;
 import me.txmc.protocolapi.PacketListener;
 import me.txmc.protocolapi.reflection.GetField;
 import net.minecraft.server.v1_12_R1.PacketPlayInChat;
+import org.bukkit.ChatColor;
 
 import java.lang.reflect.Field;
 
@@ -14,17 +15,21 @@ import java.lang.reflect.Field;
 public class ChatListener implements PacketListener {
 
     @GetField(clazz = PacketPlayInChat.class, name = "a") //The api will automatically set this field
-    private Field messageF;
+    private Field incomingMessageF;
 
     @Override
-    public void incoming(PacketEvent.Incoming incoming) throws Throwable {
-        PacketPlayInChat packet = (PacketPlayInChat) incoming.getPacket(); //This is a safe cast because we said we only wanna receive events about this packet when we registered the listener
-        String originalMessage = (String) messageF.get(packet); //Get the original message
-        messageF.set(packet, originalMessage.concat(" | This message was modified by 254n_m's packet api!")); //Set the message to a modified version of the message
+    public void incoming(PacketEvent.Incoming event) throws Throwable {
+        PacketPlayInChat packet = (PacketPlayInChat) event.getPacket(); //This is a safe cast because we said we only wanna receive events about this packet when we registered the listener
+        String originalMessage = (String) incomingMessageF.get(packet); //Get the original message
+        if (originalMessage.toLowerCase().contains("fork")) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.RED + "That word is not allowed in chat");
+            return;
+        }
+        incomingMessageF.set(packet, originalMessage.concat(" | This message was modified by 254n_m's packet api!")); //Set the message to a modified version of the message
     }
 
     @Override
-    public void outgoing(PacketEvent.Outgoing outgoing) throws Throwable {
-
+    public void outgoing(PacketEvent.Outgoing event) throws Throwable {
     }
 }
